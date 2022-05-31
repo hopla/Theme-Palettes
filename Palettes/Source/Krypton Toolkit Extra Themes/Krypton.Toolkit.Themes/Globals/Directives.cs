0f1e85e4cb1e4a9f8617bfbf0cd037567a4adeba
Peter Wagner <peterwagner@live.co.uk>
2022-05-31 10:52:54 +0100

    * New themes

---
+++ Palettes/Source/Krypton Toolkit Extra Themes/Krypton.Toolkit.Themes/Globals/Directives.cs
@@ -0,0 +1,8 @@
+﻿global using System;
+global using System.Diagnostics;
+global using System.Drawing;
+global using System.Windows.Forms;
+
+global using Microsoft.Win32;
+
+global using Krypton.Toolkit.Themes.Resources;
